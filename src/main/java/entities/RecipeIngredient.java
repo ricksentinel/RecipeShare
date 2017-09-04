596c26a6765989b6c5dff3292c575a5137a36046
Renan Kummer <renankj@gmail.com>
2017-09-04 17:26:36 -0300

    #22, #23

--- src/main/java/entities/RecipeIngredient.java
+++ src/main/java/entities/RecipeIngredient.java
@@ -4,7 +4,7 @@ package entities;
  * Ingredient's amount unit of measure.
  */
 enum MeasureUnit {
-	UNIT, GRAM, TEASPOON, TABLESPOON, CUP
+	UNIT, GRAM, KILOGRAM, TEASPOON, TABLESPOON, CUP, MILLILITER, LITER
 }
 
 
@@ -14,26 +14,36 @@ enum MeasureUnit {
  */
 public class RecipeIngredient {
 
-	String name;
-	int amount;
-	MeasureUnit measure;
-	
+	private String name;
+	private int amount;
+	private MeasureUnit unit;
+
 	/**
 	 * Constructs RecipeIngredient with default values.
 	 */
-	RecipeIngredient() {
-		name = null;
-		measure = MeasureUnit.UNIT;
-		amount = 0;
+	public RecipeIngredient() {
+		this(null, 0, MeasureUnit.UNIT);
 	}
-	
+
+	/**
+	 * Constructs RecipeIngredient with given values.
+	 * @param name Ingredient's name.
+	 * @param amount Ingredient's amount in recipe.
+	 * @param measure Amount's unit of measure.
+	 */
+	public RecipeIngredient(String name, int amount, MeasureUnit unit) {
+		this.name = name;
+		this.amount = amount;
+		this.unit = unit;
+	}
+
 	/**
 	 * @return RecipeIngredient's name.
 	 */
 	String getName() {
 		return this.name;
 	}
-	
+
 	/**
 	 * Sets RecipeIngredient's name.
 	 * @param name Name to set.
@@ -41,34 +51,35 @@ public class RecipeIngredient {
 	void setName(String name) {
 		this.name = name;
 	}
-	
+
 	/**
 	 * @return RecipeIngredient's amount.
 	 */
 	int getAmount() {
 		return this.amount;
 	}
-	
+
 	/**
 	 * Sets RecipeIngredient's amount.
-	 * @param amount
+	 * @param amount Amount to set.
 	 */
 	void setAmount(int amount) {
 		this.amount = amount;
 	}
-	
+
 	/**
 	 * @return RecipeIngredient's unit of measure.
 	 */
 	MeasureUnit getMeasure() {
-		return this.measure;
+		return this.unit;
 	}
-	
+
 	/**
 	 * Sets RecipeIngredient's unit of measure.
+	 * @param unit Ingredient amount's unit of measure.
 	 */
-	void setMeasure(MeasureUnit measure) {
-		this.measure = measure;
+	void setMeasure(MeasureUnit unit) {
+		this.unit = unit;
 	}
-	
+
 }
