e29402e860c6d82863f0f0925093a98d7820af87
Renan Kummer <renankj@gmail.com>
2017-09-04 16:20:31 -0300

    #4, #10

---
+++ src/main/java/repositories/CookingRecipeRepository.java
@@ -0,0 +1,28 @@
+package repositories;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import entities.CookingRecipe;
+
+
+/**
+ * A repository for CookingRecipe objects.
+ * @author Renan Jesus
+ */
+public interface CookingRecipeRepository extends CrudRepository<CookingRecipe, Long> {
+
+	/**
+	 * Finds recipe by its ID.
+	 * @param id ID of recipe to find.
+	 * @return CookingRecipe object if found, null otherwise.
+	 */
+	CookingRecipe findById(long id);
+
+	/**
+	 * Finds list of recipes by name.
+	 * @param name Name of recipes to find.
+	 * @return List of CookingRecipe objects if found, null otherwise.
+	 */
+	List<CookingRecipe> findByName(String name);
+
+}
