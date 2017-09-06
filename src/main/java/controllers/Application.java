62c5d52451e172130eec7a0ce9c1f7510e35b544
Renan Kummer <renankj@gmail.com>
2017-09-06 16:32:50 -0300

    #43

--- src/main/java/controllers/Application.java
+++ src/main/java/controllers/Application.java
@@ -2,10 +2,24 @@ package controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * Start RecipeShare web service.
+ * @author Renan Jesus
+ */
 @SpringBootApplication
+@ComponentScan({"controllers"})
+@EntityScan("entities")
+@EnableJpaRepositories("repositories")
 public class Application {
 
+	/**
+	 * Runs SpringBoot application.
+	 * @param args Default arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
